eefd74c78af8356a1bdd5247feef1af06a14a0f2
Kasun Dhananjaya <125995451+Kasundananjaya297@users.noreply.github.com>
2024-05-06 03:39:38 +0530

    calander modified

---
+++ src/Components/Admin/Forms/Scheduler/ModalForAddSchedule.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ModalForAddSchedule(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ModalForAddSchedule;
--- src/Components/Admin/Forms/Scheduler/ScheduleCalander.jsx
+++ src/Components/Admin/Forms/Scheduler/ScheduleCalander.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import {Calendar, momentLocalizer} from "react-big-calendar";
+import {Modal} from "react-bootstrap";
+import ModalForAddSchedule from "./ModalForAddSchedule";
 
 const localizer = momentLocalizer(moment);
 
-// Define your events list
-const myEventsList = [
-    {
-        title: 'Event 1',
-        start: new Date(2024, 3, 1), // Month is 0-indexed (April)
-        end: new Date(2024, 3, 2), // Month is 0-indexed (April)
-    },
-    {
-        title: 'Event 2',
-        start: new Date(2024, 3, 5), // Month is 0-indexed (April)
-        end: new Date(2024, 3, 6), // Month is 0-indexed (April)
-    },
-    // Add more events as needed
-];
+// // Define your events list
+// const myEventsList = [
+//     {
+//         title: 'Event 1',
+//         start: new Date(2024, 3, 1), // Month is 0-indexed (April)
+//         end: new Date(2024, 3, 2), // Month is 0-indexed (April)
+//     },
+//     {
+//         title: 'Event 2',
+//         start: new Date(2024, 3, 5), // Month is 0-indexed (April)
+//         end: new Date(2024, 3, 6), // Month is 0-indexed (April)
+//     },
+//     // Add more events as needed
+// ];
 function ScheduleCalander(props) {
+
+    const [eventList, setEventList] = useState([])
+    //hook for add new event
+    const[showModalAddEvent, setShowModalAddEvent] = useState(false)
     return (
         <div>
             <Calendar
                 localizer={localizer}
-                events={myEventsList}
+                events={eventList}
                 startAccessor="start"
                 endAccessor="end"
+                onSelectEvent={event => {console.log("clicked event", event)}}
+                onSelectSlot={() => {setShowModalAddEvent(true)}}
+                selectable
                 style={{ height: 800 }}
             />
+            <Modal show={showModalAddEvent} onHide={()=>{setShowModalAddEvent(false)}}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create New Schedule</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ModalForAddSchedule />
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
